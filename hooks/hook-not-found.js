d0a0e38f9a0fa96372ebff8899153375f49cd6be
CokeBeliever <cokebeliever@163.com>
2023-01-02 23:23:00 +0800

    feat: if requests are not found, return 404.html

--- hooks/hook-not-found.js
+++ hooks/hook-not-found.js
@@ -1,12 +1,20 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const mime = require("mime");
+const { STATIC_PATH } = require("../config/paths");
 
 /**
  * not found 处理
  * @param { http.IncomingMessage } req
- * @param { http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage } } res
+ * @param { http.ServerResponse } res
  */
-module.exports = function (req, res) {
+module.exports = async function (req, res) {
+  // 如果已响应，就直接 return
+  if (res.writableEnded) return;
+  const resourcePath = path.resolve(STATIC_PATH, "404.html");
+
   res.statusCode = 404;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("not found");
+  res.setHeader("Content-Type", mime.getType(resourcePath));
+  res.end(fs.readFileSync(resourcePath));
 };
--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const { hookNotFound } = require("./hooks/");
 const { hostname, port } = require("./config/server");
 
 const server = http.createServer(async (req, res) => {
-  try {
-    switch (true) {
-      // 如果 url 以 /api 开头，表示请求应用程序接口
-      case /^\/api/.test(req.url):
-        // todo ...
-        break;
-      // 否则，表示请求静态资源
-      default:
-        break;
-    }
-  } catch (e) {}
+  switch (true) {
+    // 如果 url 以 /api 开头，表示请求应用程序接口
+    case /^\/api/.test(req.url):
+      // todo ...
+      break;
+    // 否则，表示请求静态资源
+    default:
+      break;
+  }
 
   // not found
   await hookNotFound(req, res);
